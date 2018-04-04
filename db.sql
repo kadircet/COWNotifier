3b48fa0c45dbdcad657d2274562481adf94e4af8
kadir cetinkaya <kadircetinkaya.06.tr@gmail.com>
2018-04-04 14:17:05 +0300

    Introduce alias table.

--- db.sql
+++ db.sql
@@ -7,12 +7,19 @@ CREATE TABLE IF NOT EXISTS `users` (
 	primary key(id)
 );
 
-DROP TABLE `topics`;
-CREATE TABLE `topics` (
+CREATE TABLE IF NOT EXISTS `topics` (
 	id int not null auto_increment,
 	cid int not null,
 	topic varchar(255) not null,
 	primary key(id),
 	foreign key(cid) references users(cid),
 	unique `ident` (`cid`, `topic`)
+);
+
+CREATE TABLE IF NOT EXISTS `aliases` (
+  `id` int not null auto_increment,
+  `cid` int not null,
+  `alias` varchar(255) not null,
+  foreign key(cid) references users(cid),
+  primary key(`id`)
 );
