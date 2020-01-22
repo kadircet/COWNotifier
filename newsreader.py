9d62f08a7ce4fb9fb668e03f328d68090bed8d6b
hbostann <hbostann@gmail.com>
2020-01-22 23:26:25 +0300

    improve request handling

--- newsreader.py
+++ newsreader.py
@@ -16,34 +16,50 @@ class newsReader:
         self.initialized = False
         self.initConnection()
     
-    def makeAPICall(self,endpoint, params={}, post=False, timeout=30):
+    # TODO: Make this function higher level
+    def makeAPICall(self, endpoint, params={}, **kwargs):
+        post = kwargs.get('post', False)
+        timeout = kwargs.get('timeout', 30)
+        retry = kwargs.get('retry', False)
+        redirect = kwargs.get('redirect', True)
+        backoff = 1
         if not endpoint.endswith('/'):
             endpoint += '/'
-        try:
-            req_url = self.conparams[0]+endpoint
-            # do POST request
-            if post:
-                resp = requests.post(req_url, data=params, cookies=self.token, timeout=timeout)
-            else: # do GET request
-                resp = requests.get(req_url, params=params, cookies=self.token, timeout=timeout)
-            return resp
-        except Exception as e:
-            print(e, datetime.datetime.now())
-            traceback.print_exc()
-            sys.exit(1)
-
-        
-
+        req_url = self.conparams[0]+endpoint
+        while True:
+            try:
+                # do POST request
+                if post:
+                    resp = requests.post(req_url, data=params,
+                                         cookies=self.token, timeout=timeout,
+                                         allow_redirects=redirect)
+                else: # do GET request
+                    resp = requests.get(req_url, params=params,
+                                        cookies=self.token, timeout=timeout,
+                                        allow_redirects=redirect)
+                resp.raise_for_status()
+                return resp, resp.status_code
+            except requests.exceptions.RequestException as e:
+                print(f"[X] Request Failed: {e} -- Endpoint: {endpoint}, Data: {params}")
+                resp = None
+                if not retry:
+                    return resp, e.response.status_code
+                print(f"[*] Retrying in {backoff} seconds")
+                time.sleep(backoff)
+                backoff = max(60, backoff*2)
+            except Exception as e:
+                # Something outside "requests" library failed
+                print(e, datetime.datetime.now())
+                traceback.print_exc()
+                sys.exit(1)
+                
     def initConnection(self):
         self.updateAuthToken()
         self.time = time.time()
 
         # Grab category ids and names
         self.categories = {}
-        resp = self.makeAPICall("site.json")
-        if not resp.ok:
-            # TODO: recover
-            sys.exit(2)
+        resp, _ = self.makeAPICall("site.json", retry=True)
         resp_categories = resp.json()['categories']
         for item in resp_categories:
             self.categories[item['id']] = item['name']
@@ -58,13 +74,10 @@ class newsReader:
             traceback.print_exc()
 
         # Update the last stored post id
-        resp = self.makeAPICall('posts.json', params={'before':0})
-        if not resp.ok:
-            # TODO: recover
-            sys.exit(2)
-        posts = resp.json()['latest_posts']
-        remote_post_id = max([p['id'] for p in posts])
         if self.last_post == None:
+            resp, _ = self.makeAPICall('posts.json', params={'before':0}, retry=True)
+            posts = resp.json()['latest_posts']
+            remote_post_id = max([p['id'] for p in posts])    
             with open(self.lfile, 'w') as f:
                 f.write(str(remote_post_id))
             self.last_post = remote_post_id
@@ -73,27 +86,16 @@ class newsReader:
     def updateAuthToken(self):
         self.token = None
         data={"username":self.conparams[2], "password":self.conparams[3]}
-        backoff = 1
-        while True:
-            try:
-                resp = requests.post(self.conparams[4], data=data, allow_redirects=False, timeout=30)
-                resp.raise_for_status()
-                for cookie in resp.headers['Set-Cookie'].split(";"):
-                    cookie = cookie.strip()
-                    if cookie.startswith("_t="):
-                        self.token = {"_t":cookie[3:]}
-                        self.time =  time.time()
-                        print(f'[*] New Auth Token: {self.token} acquired at {self.time}')
-                        break
+        resp, _ = self.makeAPICall(self.conparams[4],params=data,
+                                post=True, retry=True,
+                                redirect=False)
+        for cookie in resp.headers['Set-Cookie'].split(";"):
+            cookie = cookie.strip()
+            if cookie.startswith("_t="):
+                self.token = {"_t":cookie[3:]}
+                self.time =  time.time()
+                print(f'[*] New Auth Token: {self.token} acquired at {self.time}')
                 break
-            except requests.exceptions.RequestException as e:
-                print(f'[X] Failed AUTH: {e.response.status_code}, Retrying in {backoff} seconds')
-                time.sleep(backoff)
-                backoff = min(60, backoff * 2)
-            except Exception as e:
-                print('[X] Failed AUTH!')
-                print(e, datetime.datetime.now())
-                sys.exit(1)
 
     def getIdForTopic(self, topic):
         for cat_id, name in self.categories.items():
@@ -114,35 +116,40 @@ class newsReader:
 
     def updatePosts(self, mention_manager):
         if not self.initialized:
-            return []
+            return {}
         # TODO: Find token expiration, 60 secs is a bit low
         if time.time() - self.time > 60.:
             self.updateAuthToken()
         
         # Get latest posts
-        resp = self.makeAPICall('posts.json', params={'before':0})
-        if not resp.ok:
-            # TODO: recover
-            sys.exit(2)
+        resp, _ = self.makeAPICall('posts.json', params={'before':0})
+        if resp == None:
+            return {}
         posts = json.loads(resp.text)['latest_posts']
         last = max([p['id'] for p in posts])
         start = self.last_post
         res = {}
-        while start <= last:
+        while start < last:
             start += 1
             # Get post
-            post = self.makeAPICall(f'posts/{start}.json')
-            if not post.ok:
-                # TODO: recover
-                continue
-            print(f"Post[{start}]: {post.ok}, {post.status_code}", end=" - ")
+            post, code = self.makeAPICall(f'posts/{start}.json')
+            if post == None:
+                if code == 403:
+                    continue
+                else:
+                    break
             post = post.json()
+            # post information we get through 'posts/{id}.json'
+            # doesn't contain category_id and topic_title so
+            # we need to make another request for that info.
+
             # Get post's topic for category and title
-            topic = self.makeAPICall(f"t/{post['topic_id']}.json")
-            if not topic.ok:
-                # TODO: recover
-                continue
-            print(f"Topic[{start}]: {topic.ok}, {topic.status_code}")
+            topic, code = self.makeAPICall(f"t/{post['topic_id']}.json")
+            if topic == None:
+                if code == 403:
+                    continue
+                else:
+                    break
             topic = topic.json()
             if topic['category_id'] not in res:
                 res[topic['category_id']] = []
@@ -150,8 +157,8 @@ class newsReader:
                                                          self.categories[topic['category_id']],
                                                          topic['title'],
                                                          post['created_at'],
-                                                         post['raw'],
-                                                         post['cooked'],
+                                                         post['raw'],       # raw msg (markdown)
+                                                         post['cooked'],    # mgs in html format
                                                          mention_manager))
         self.last_post = start
         with open(self.lfile, 'w') as f:
